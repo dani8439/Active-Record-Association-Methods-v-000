eb7e55995480a88e347b2d739d0ef023044a92bc
Dani Schuhman <dani8439@aol.com>
2018-08-10 20:13:21 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    s = []
     self.artists.map {|name| artists.name << }
   end
 end
